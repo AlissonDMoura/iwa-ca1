4283f05fffbd5fe901c801db1e120b6c11aa185f
AlissonDMoura <alissonmaster@hotmail.com>
2022-05-17 10:51:26 +0000

    Debuging _show

--- ca2/server/services/render.js
+++ ca2/server/services/render.js
@@ -1,7 +1,16 @@
-exports.homeRoutes = (req, res) => {
-    res.render('index');
-}
+const axios = require('axios');
 
+exports.homeRoutes = (req, res) => {
+    //get request to the /api/users
+    axios.get('https://3000-alissondmoura-iwaca1-tjn95c347rn.ws-eu45.gitpod.io/api/users')
+    .then(function(response){
+        res.render('index',{users:response.data});
+    })
+    .catch(err =>{
+        res.send(err);
+    })
+
+}
 exports.add_dish = (req, res) => {
     res.render('add_dish');
 }
