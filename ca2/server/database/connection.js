e4d3a4c2b564b2c0636ae70b6490d84a5f0eebbd
AlissonDMoura <alissonmaster@hotmail.com>
2022-05-17 04:13:04 +0000

    mongoDB con debug

--- ca2/server/database/connection.js
+++ ca2/server/database/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const conectDB = async()=>{
+const connectDB = async()=>{
     try{
 //mongodb connection string
+
 const con = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology:true,
-    useFindAndModify:false,
-    useCreateIndex:true})
+    useUnifiedTopology: true,
+   })
 
     console.log('MongoDB connected: ${con.connection.host}');
     }catch(err){
